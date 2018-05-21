5ceda6cc5d130b03bd15041e1da766bf2327feb8
Sim Tze Jan <tzejan@gmail.com>
2018-05-21 15:05:14 +0800

    PortfolioPreview is now a class, does a fetch, provide props to PortofolioDescription

--- src/components/PortfolioDescription.js
+++ src/components/PortfolioDescription.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const PortfolioDescription = () => (
+const PortfolioDescription = (props) => (
   <div className="card m-4">
     <div className="card-body">
       <h3>StashAway's standard portfolios:</h3>
@@ -9,19 +9,19 @@ const PortfolioDescription = () => (
           <div className="col-sm">
             <div className="descHeader">Expected annual returns:</div>{" "}
             <div className="descValue">
-              <h2>6.4%</h2>
+              <h2>{props.portfolio_description.expected_annual_returns}</h2>
             </div>
           </div>
           <div className="col-sm">
             <div className="descHeader">VaR:</div>
             <div className="descValue">
-              <h2>3.4%</h2>
+              <h2>{props.portfolio_description.VaR}%</h2>
             </div>
           </div>
           <div className="col-sm">
             <div className="descHeader">Optimised for:</div>
             <div className="descValue">
-              <h2>USD</h2>
+              <h2>{props.portfolio_description.optimised_currency}</h2>
             </div>
           </div>
         </div>
--- src/components/PortfolioPreview.js
+++ src/components/PortfolioPreview.js
@@ -1,12 +1,104 @@
-import React from 'react';
-import PortfolioDescription from './PortfolioDescription';
-import PortfolioComposition from './PortfolioComposition';
+import React, { Component } from "react";
+import PortfolioDescription from "./PortfolioDescription";
+import PortfolioComposition from "./PortfolioComposition";
 
-const PortfolioPreview = () => (
-    <div className="portfolio-preview">
-        <PortfolioDescription/>
-        <PortfolioComposition/>
-    </div>
-);
+class PortfolioPreview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      portfolio_description: {
+        expected_annual_returns: 6.4,
+        VaR: 3.4,
+        optimised_currency: "USD"
+      },
+      global_currency_exposure: [
+        {
+          currency: "USD",
+          exposure: 30
+        },
+        {
+          currency: "SGD",
+          exposure: 30
+        },
+        {
+          currency: "EUR",
+          exposure: 20
+        },
+        {
+          currency: "JPY",
+          exposure: 10
+        },
+        {
+          currency: "MYR",
+          exposure: 5
+        },
+        {
+          currency: "CNY",
+          exposure: 5
+        }
+      ],
+      asset_allocation: [
+        {
+          asset_group_name: "US EQUITIES",
+          assets: [
+            {
+              asset_name: "US Equities (Total)",
+              ticker: "VTI",
+              exposure: 5
+            },
+            {
+              asset_name: "US Equities (Dividends)",
+              ticker: "VIG",
+              exposure: 6
+            },
+            {
+              asset_name: "US Equities (Small-Cap, Growth)",
+              ticker: "VBK",
+              exposure: 1.2
+            }
+          ]
+        },
+        {
+          asset_group_name: "International EQUITIES",
+          assets: [
+            {
+              asset_name: "Asia ex-Japan Equities",
+              ticker: "VTI",
+              exposure: 12.3
+            },
+            {
+              asset_name: "China Equities",
+              ticker: "PGJ",
+              exposure: 0.2
+            },
+            {
+              asset_name: "US Total Stock",
+              ticker: "VBK",
+              exposure: 23.7
+            }
+          ]
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/api/portfolio")
+      .then(response => response.json())
+      .then(jsondata => console.log(jsondata))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="portfolio-preview">
+        <PortfolioDescription
+          portfolio_description={this.state.portfolio_description}
+        />
+        <PortfolioComposition />
+      </div>
+    );
+  }
+}
 
 export default PortfolioPreview;
